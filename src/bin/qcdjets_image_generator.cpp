ed2b3815d316652e7ce7e6c02a68183667d94df8
ustcllh <usctllh@gmail.com>
2019-05-23 12:58:39 -0500

    add inclusive jets image generator

--- src/bin/qcdjets_image_generator.cpp
+++ src/bin/qcdjets_image_generator.cpp
@@ -9,22 +9,21 @@
 #include "Pixeliser.h"
 
 int main(int argc, char* argv[]){
-  if(argc != 3){
-    std::cout << "Usage: ./image_generator <outDir> <out_prefix>" << std::endl;
+  if(argc != 2){
+    std::cout << "Usage: ./image_generator <outDir>" << std::endl;
     exit(1);
   }
 
   // configuration
-  double pthat = 150.;
+  double pthat = 120.;
   int nEvents = 100;
 
   // output
   std::string outdir = argv[1];
-  std::string prefix = argv[2];
 
   // Jet definition
   double R = 0.4;
-  double ptmin = 160.;
+  double ptmin = 130.;
   fastjet::JetDefinition jet_def(fastjet::antikt_algorithm, R);
 
   // jets container
@@ -35,7 +34,8 @@ int main(int argc, char* argv[]){
   pythiaEvent pythia(pthat, -3, 3, false);
 
   // count for jet images
-  int njet = 0;
+  int nq=0;
+  int ng=0;
 
   for(int i=0; i<nEvents; i++){
     qjets.clear();
@@ -53,8 +53,31 @@ int main(int argc, char* argv[]){
       if(!(it->has_structure())) jets.erase(it);
     }
 
+    // matching and tagging
+    for(auto&& p : initiators){
+      float dr_min = 9999;
+      fastjet::PseudoJet j_matched;
+      for(auto&& jet : jets){
+        float dr = jet.delta_R(p);
+        if(dr < dr_min){
+          dr_min = dr;
+          j_matched = jet;
+        }
+      }
+      if(dr_min>0.4) continue;
+
+      // quark/gluon jet tagging
+      auto pid = p.user_info<MyInfo>().pdg_id();
+      if(pid == 21){
+        gjets.push_back(j_matched);
+      }
+      else{
+        qjets.push_back(j_matched);
+      }
+    } // loop over initiators
+
     // generate image
-    for(auto&& jet: jets){
+    for(auto&& jet: gjets){
       pixeliser<float> p (5, 33, 33);
       p.set_title("E;Multi.;|px|;|py|;|pz|");
       float max;
@@ -72,12 +95,38 @@ int main(int argc, char* argv[]){
         p.fill(4, 0.4, deta, dphi, std::abs(particle.pz()));
       } // loop over particles
       p.set_maximum(max);
-      p.plot(outdir, prefix + std::to_string(njet));
+      p.plot(outdir, "gluon_jet_" + std::to_string(ng));
       // p.print();
-      p.save(outdir, prefix + std::to_string(njet));
+      p.save(outdir, "gluon_jet_" + std::to_string(ng));
 
-      njet++;
-    } // loop over  jets
+      ng++;
+    } // loop over gluon jets
+
+    for(auto&& jet: qjets){
+      pixeliser<float> p (5, 33, 33);
+      p.set_title("E;Multi.;|px|;|py|;|pz|");
+      float max;
+      for(auto&& particle : jet.constituents()){
+        float deta = particle.eta() - jet.eta();
+        float dphi = particle.phi() - jet.phi();
+        // std::cout << deta << "\t" << dphi << "\t" << std::endl;
+
+        max = max>particle.e()? max : particle.e();
+
+        p.fill(0, 0.4, deta, dphi, particle.e());
+        p.fill(1, 0.4, deta, dphi, 1.);
+        p.fill(2, 0.4, deta, dphi, std::abs(particle.px()));
+        p.fill(3, 0.4, deta, dphi, std::abs(particle.py()));
+        p.fill(4, 0.4, deta, dphi, std::abs(particle.pz()));
+      } // loop over particles
+      p.set_maximum(max);
+      p.plot(outdir, "quark_jet_" + std::to_string(nq));
+      // p.print();
+      p.save(outdir, "quark_jet_" + std::to_string(nq));
+      nq++;
+    } // loop over gluon jets
+
+
 
   } // loop over events
 
